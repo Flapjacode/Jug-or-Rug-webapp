746f20d21633a031855c6f0ee32c37bd02430b75
Flapjacode <123619120+Flapjacode@users.noreply.github.com>
2024-05-14 16:29:03 -0700

    add vs

--- src/main/java/http/www/thymeleaf/org/JugorRug/JugOrRugApplication.java
+++ src/main/java/http/www/thymeleaf/org/JugorRug/JugOrRugApplication.java
@@ -3,11 +3,43 @@ package http.www.thymeleaf.org.JugorRug;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Random;
+
+
 @SpringBootApplication
 public class JugOrRugApplication {
-
-	public static void main(String[] args) {
+  	public static void main(String[] args) {
 		SpringApplication.run(JugOrRugApplication.class, args);
-	}
+	// JugnRug3Controller.java
+
+@Controller
+public class JugnRug3Controller {
+    private Random random = new Random();
+    
+  
+
+    @GetMapping("/")
+    public String showGame() {
+        return "game";
+    }
+
+    @PostMapping("/play")
+    public String playGame(@RequestParam String choice, Model model) {
+        boolean isJug = random.nextBoolean();
+        String result = isJug ? "Jug" : "Rug";
+
+        model.addAttribute("result", result);
+        model.addAttribute("win", result.equals(choice));
+
+        return "game";
+    }
+}
+}
 
 }
