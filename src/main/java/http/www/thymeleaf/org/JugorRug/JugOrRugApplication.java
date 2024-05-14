f98052c5fb16ef083b47768d9b7cd1f4050b561b
Flapjacode <123619120+Flapjacode@users.noreply.github.com>
2024-05-14 14:35:17 -0700

    init

---
+++ src/main/java/http/www/thymeleaf/org/JugorRug/JugOrRugApplication.java
@@ -0,0 +1,13 @@
+package http.www.thymeleaf.org.JugorRug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JugOrRugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JugOrRugApplication.class, args);
+	}
+
+}
---
+++ src/test/java/http/www/thymeleaf/org/JugorRug/JugOrRugApplicationTests.java
@@ -0,0 +1,13 @@
+package http.www.thymeleaf.org.JugorRug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JugOrRugApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
